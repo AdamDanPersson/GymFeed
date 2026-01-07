5101980d900c895748ae955553b0eb8731f93b32
Adam Persson <adamdanpersson@gmail.com>
2026-01-07 09:56:58 +0100

    Add routing and start page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import flowMark from '../assets/airwave.svg'
+import statsMark from '../assets/bar_chart.svg'
+import './Home.css'
+
+function HomePage() {
+  return (
+    <section className="landing-screen" aria-label="Välj arbetsläge">
+      <Link to="/projekt" className="landing-panel landing-panel--flow">
+        <img src={flowMark} alt="Flow logotyp" />
+        <span>Flow</span>
+      </Link>
+      <Link to="/stats" className="landing-panel landing-panel--stats">
+        <img src={statsMark} alt="Stats logotyp" />
+        <span>Stats</span>
+      </Link>
+    </section>
+  )
+}
+
+export default HomePage
