f0c408c0ba90dd9c1fd682f2860e619d2a3a664e
Adam Persson <adamdanpersson@gmail.com>
2026-01-09 08:30:27 +0100

    Fix CORS: allow x-user-id header

--- server/src/index.js
+++ server/src/index.js
@@ -35,8 +35,8 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id']
 }
 
 app.use(cors(corsOptions))
